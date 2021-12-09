277e006f30bf6dcce1baa58033e4711bbe510db6
sam1818 <samuelesciatore.18@gmail.com>
2021-12-09 15:29:09 +0100

    Temp fix to upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+ mode: 'jit',
+ purge: [
+  './pages/**/*.{js,ts,jsx,tsx}',
+  './components/**/*.{js,ts,jsx,tsx}',
+  './base/**/*.{js,ts,jsx,tsx}',
+ ],
+ darkMode: 'class',
+ theme: {
+  extend: {
+   colors: {
+    background: '#1f1f1f',
+    'baltic-sea': '#29262B',
+    'ship-gray': '#3C3541',
+    'medium-purple': '#AC5FDB',
+    'biloba-flower': '#E3A2EE',
+   },
+  },
+ },
+ variants: {
+  extend: {},
+ },
+ plugins: [],
+}
