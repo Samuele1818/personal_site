ed63144b6e4db3efb2961a5f277c6ab1c120f50b
sam1818 <samuelesciatore.18@gmail.com>
2021-12-09 16:10:52 +0100

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app'
 import { appWithTranslation } from 'next-i18next'
 import { ThemeContextProvider } from '../lib/ThemeProvider/ThemeProvider'
-import '../styles/globals.css'
+import '/styles/globals.css'
 
 const MyApp = ({
  Component,
